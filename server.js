f155d8102ad52d352fc8f77a453f433b24749a27
System <none>
2017-09-25 15:32:58 +0000

    [Add] Shorten Url implementation

--- server.js
+++ server.js
@@ -4,34 +4,98 @@
 // init project
 var express = require('express');
 var app = express();
+var ObjectId = require('mongodb').ObjectID;
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
+var MongoClient = require("mongodb").MongoClient;
+var mongodbUrl = "mongodb://louis719:deathbrand@ds133211.mlab.com:33211/freecodecamp_pratice";
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
+app.get("/:shortUrl", function(request, response, next){
+  var url = request.params.shortUrl
+  console.dir( request.query )
+  console.log( "url :" + url )
+  try {
+    var mongoDbId = ObjectId(url)
+  } catch (err) {
+    response.end( "invalid shorten url: " + url )
+  }
+  
+  MongoClient.connect(mongodbUrl, function (err, db){
+      console.log("connect ok")
+      if( err ){
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+        response.end( "Connect to db fail" );
+        next()
+      }
+      else{
+        db.collection('shortUrl').findOne( {"_id":mongoDbId}, function(err, document ){
+            console.dir(document)
+            if( document ){
+              console.log("find shorten Url... redirect to " + document.originUrl)
+              response.writeHead( 302, {Location: document.originUrl} )
+              response.end()
+            }
+            else{
+              next()
+            }
+        })
+      }
+  })
+})
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
+app.get("/shortenUrl", function (request, response) {
+  console.log( "4" )
+  var url = request.query.url  
+  
+  var returnObj = {}
+  returnObj.original_url = url
+  
+  MongoClient.connect(mongodbUrl, function (err, db) {    
+    
+    var urlExist = false
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+      response.end( "Connect to db fail" );
+      return
+    } 
+    else {
+      db.collection('shortUrl').findOne( { "originUrl": url }, function(err, document){
+        if( document ){
+          console.dir( document );
+          returnObj.shorten_url = document._id
+          console.log( JSON.stringify(returnObj) )
+          response.end( JSON.stringify(returnObj) )
+          db.close();
+        }
+        else{
+            db.collection("shortUrl").insert( { originUrl: url }, function(err, result){
+              if( err ){
+                console.error( err );
+              }
+              else{
+                console.dir(result)
+                returnObj.shorten_url = result.insertedIds[0]
+                response.end( JSON.stringify(returnObj) )
+              }
+              db.close();
+            } );
+        }
+      } );
+    }          
+    
+  });
+  
 });
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
